bade001794429e5b9a7c6db8d7a99ae6f6a77148
shinciako <shinciako@gmail.com>
2023-04-26 01:57:34 +0200

    main window impl

--- MessagePuzzle/Form1.Designer.cs
+++ MessagePuzzle/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            instrukcja = new Label();
+            label2 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Top;
+            progressBar1.Location = new Point(150, 10);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(703, 20);
+            progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button1.BackColor = Color.SpringGreen;
+            button1.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 44);
+            button1.TabIndex = 1;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(14, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 27);
+            label1.TabIndex = 2;
+            label1.Text = "15:00";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Location = new Point(869, 10);
+            tableLayoutPanel1.MaximumSize = new Size(100, 100);
+            tableLayoutPanel1.MinimumSize = new Size(100, 100);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(100, 100);
+            tableLayoutPanel1.TabIndex = 4;
+            // 
+            // instrukcja
+            // 
+            instrukcja.AutoSize = true;
+            instrukcja.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            instrukcja.Location = new Point(153, 92);
+            instrukcja.Name = "instrukcja";
+            instrukcja.Size = new Size(72, 18);
+            instrukcja.TabIndex = 5;
+            instrukcja.Text = "Instrukcja";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(153, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 22);
+            label2.TabIndex = 6;
+            label2.Text = "Instrukcja";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(984, 461);
+            Controls.Add(label2);
+            Controls.Add(instrukcja);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(progressBar1);
+            MinimumSize = new Size(800, 500);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBar1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label instrukcja;
+        private Label label2;
     }
 }
--- MessagePuzzle/Form1.cs
+++ MessagePuzzle/Form1.cs
@@ -1,10 +1,49 @@
+using System.Diagnostics;
+
 namespace MessagePuzzle
 {
     public partial class Form1 : Form
     {
+
+        private DateTime startTime;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            instrukcja.Text = "Za zadanie masz to i tamto";
+            progressBar1.Left = (this.ClientSize.Width - progressBar1.Width) / 2;
+            progressBar1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Top = progressBar1.Height + 10;
+            tableLayoutPanel1.Left = this.ClientSize.Width - button1.Width - 10;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = @"https://pastebin.com/Vi1zGupx", UseShellExecute = true });
+            startTime = DateTime.Now;
+            timer1.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            int remainingSeconds = (int)Math.Max(0, (15 - elapsedTime.TotalSeconds));
+            label1.Text = TimeSpan.FromSeconds(remainingSeconds).ToString(@"mm\:ss");
+
+            if (remainingSeconds == 0)
+            {
+                label1.Text = "15:00";
+                timer1.Enabled = false;
+                progressBar1.Value = 0;
+                button1.Enabled = true;
+            }
         }
     }
 }
